59f0c5f3b9f56ddf571dde9a0eae7f9fda0e0341
Christopher Newell <cbnewell@byui.edu>
2020-11-09 21:30:01 -0600

    Complete Tutorial

---
+++ Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Legend of Johnny Lingo",
+                        ReleaseDate = DateTime.Parse("2004-5-11"),
+                        Genre = "Family",
+                        Rating = "G",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Meet The Mormons",
+                        ReleaseDate = DateTime.Parse("2014-8-10"),
+                        Genre = "Documentary",
+                        Rating = "PG",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Saints and Soldiers",
+                        ReleaseDate = DateTime.Parse("2003-9-11"),
+                        Genre = "Action",
+                        Rating = "PG-13",
+                        Price = 9.99M
+                    }
+
+                ); 
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace MvcMovie
 {
@@ -21,9 +25,22 @@ namespace MvcMovie
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies 
+                // is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +69,7 @@ namespace MvcMovie
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
